1c3fb2d0c1b99837a926aa6e2b887fe4b0aaebfc
Arhan <paintingsalamander@gmail.com>
2025-01-15 01:55:07 +0530

    Fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 
 // Database Connection
-mongoose.connect('mongodb://localhost:27017/taskManager', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
